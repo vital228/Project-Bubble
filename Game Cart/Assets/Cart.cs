579f0b01165122d34fd9c984feb10421d3e4c70a
Vitaly <31014393+vital228@users.noreply.github.com>
2018-07-19 16:40:52 +0300

    add project

---
+++ Game Cart/Assets/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cart : MonoBehaviour {
+    bool onTouch = false;
+    void OnMouseUp()
+    {
+        onTouch = !onTouch;
+    }
+    // Update is called once per frame
+    void Update () {
+       
+        if (onTouch)
+        {
+            Vector3 worldPos = Input.mousePosition;
+            worldPos = Camera.main.ScreenToWorldPoint(worldPos);
+            this.transform.position = new Vector3(worldPos.x,worldPos.y,1);
+        }
+	}
+}
---
+++ Game Cart/Assets/Script/LoadScene36.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName: "36");
+    }
+}
